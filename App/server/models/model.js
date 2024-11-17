8d709de2814e483a681f1521a6e2faba5c2e7b86
PatJNewellSHU <c1013103@hallam.shu.ac.uk>
2024-11-17 13:25:31 -0800

    added models

--- App/server/models/model.js
+++ App/server/models/model.js
@@ -5,19 +5,41 @@ var connection = require('./connection');
 // allows insertion
 // selection
 // deletion
+// updating
 
 class model extends connection{
     constructor(){
         super('MySqlSAAD','Group21','35.246.42.167','saaddb','3306');
     }
-    async select(table,columns,where){
-
+    async select(table,columns,where){ 
+        let conn = await this.createTcpPool(); // creation connection to database
+        let result; // decalre resut variable
+        conn.query('SELECT  '+ columns + ' FROM ' + table + 'WHERE ' + where, function(err, results, fields) { // query database
+        if (err) throw esrr;
+        result = results; // return results
+    });
+    return result
     }
-    async insert(table, columns,entry){
-
+    async insert(table, entry){
+        let conn = await this.createTcpPool(); // creation connection to database
+        conn.query('INSERT INTO '+ table +'SET ?', entry, function(err, results) {    // query database using sql
+            if (err) throw err; // error handle
+            console.log('Inserted Row ID:', results.insertId); // confirmation of query 
+        });
     }
-    async delete(){
-
+    async delete(table, where){
+        let conn = await this.createTcpPool();
+        conn.query('DELETE FROM ' + table + ' WHERE ' + where  ,function(err, results) {
+            if (err) throw err;
+            console.log('DELETED');
+        });
+    }
+    async update(table,value,where){
+        let conn = await this.createTcpPool();
+        conn.query('UPDATE ' + table + ' SET ' + value + ' WHERE ' + where,function(err, results) {
+            if (err) throw err;
+            console.log('updated');
+        });
     }
 }
 
