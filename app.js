50196f5acd998afbc76fa52d6099d4e4d357420e
GitHoint <ozzyjosh03@gmail.com>
2024-12-02 16:57:25 +0000

    hashing setup

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require("express");
 const path = require("path")
 const app = express();
 const registration = require("./App/server/controllers/registration");
+const bcrypt = require('bcrypt');
+var session = require('express-session');
+
+
 app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//hashing
+const saltRounds = 10;
 
 //Roots
 app.get("/", (req, res) => {
@@ -24,7 +30,7 @@ app.get("/login", (req, res) => {
 
 //Posts
 
-app.post("/register", (req, res) => {
+app.post("/register",  async (req, res) => {
     var password = null;
     const registerData = {
         name: req.body.firstname + req.body.surname,
@@ -34,13 +40,25 @@ app.post("/register", (req, res) => {
     }
     if (req.body.password == req.body.confirm) {
         password = req.body.password;
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            bcrypt.hash(password, salt, function (err, hash) {
+                const register = new registration();
+                console.log(hash);
+                register.register(registerData.name, hash, "member", registerData.email, registerData.birthday, registerData.phone)
+                res.render("login");
+            })
+        })
     }
-    const register = new registration();
-    register.register(registerData.name, password, "member", registerData.email, registerData.birthday, registerData.phone)
 })
 
-
+app.post("/login", async (req, res) => {
+    loginData = {
+        email: req.body.email,
+        password: req.body.password
+    }
+})
 const port = 8080;
 app.listen(port, () => {
     console.log(`listening on port: ${port} `)
+
 })
