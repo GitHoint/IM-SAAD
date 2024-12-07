1587f352fd6d22df0bc93a0a34ca915bfd01500b
PatJNewellSHU <c1013103@hallam.shu.ac.uk>
2024-12-07 07:17:41 -0800

    searchmedia

--- App/unitTesting/search.test.js
+++ App/unitTesting/search.test.js
@@ -2,7 +2,7 @@
 test('Test that the search controller media search method returns an object from the database when first id is queries| Author: Patrick Newell', async () =>{
     let search = require('../server/controllers/search');
     let searcher = new search();
-    searcher.mediaSearch("mediaId = 1", function(res){
+    searcher.searchMedia("mediaId = 1", function(res){
         console.log(res);
     });
 });
