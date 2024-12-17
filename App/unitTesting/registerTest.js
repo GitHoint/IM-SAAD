4f028df60a51d2cba9d7c37129e836edf8378d05
GitHoint <ozzyjosh03@gmail.com>
2024-12-17 13:37:29 +0000

    test cases

--- App/unitTesting/registerTest.js
+++ App/unitTesting/registerTest.js
@@ -1,6 +1,6 @@
 const reg = require('../server/controllers/registration');
 let register = new reg();
 
-console.log('Test to add a new user to the database should log a success message | Author: Patrick Newell')
+console.log('Test to add a new user to the database should log a success message | Author: Joshua Osborne')
 register.register("bob","bob","bob","bob","bob","bob");
 console.log("Test Passed")
