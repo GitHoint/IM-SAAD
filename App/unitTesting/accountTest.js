e9ee4797fe80ff39c7385f7810e16e43d861ac96
Olivert32 <c2077500@hallam.shu.ac.uk>
2024-12-17 13:50:59 +0000

    created account details unit test

--- App/unitTesting/accountTest.js
+++ App/unitTesting/accountTest.js
@@ -1,7 +1,7 @@
 const account = require("../server/controllers/account");
 let manager = new account();
 
-console.log("Test to see if all account details cna be changed for a user | Author: Patrick Newel")
+console.log("Test to see if all account details cna be changed for a user | Author: Oliver Topley")
 
 manager.email("newEmail@email.com",20);
 
