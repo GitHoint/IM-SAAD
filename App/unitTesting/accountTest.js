937ae9e03b2e0a27f9eb029f48c9a050fea273f1
Olivert32 <c2077500@hallam.shu.ac.uk>
2024-12-18 17:03:37 +0000

    fixed error in unit test

--- App/unitTesting/accountTest.js
+++ App/unitTesting/accountTest.js
@@ -1,11 +1,9 @@
 const account = require("../server/controllers/account");
 let manager = new account();
 
-<<<<<<< HEAD
 console.log("Test to see if all account details cna be changed for a user | Author: Oliver Topley")
-=======
-console.log("Test to see if all account details cna be changed for a user | Author: Patrick Newell")
->>>>>>> bffb91f0d78c885b878a1bc2f1a71dd4b2b9b7b7
+
+
 
 manager.email("newEmail@email.com",20);
 
